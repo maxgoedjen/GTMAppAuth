907ae9ca19c290932b075a492a55abafc798dbf2
Max Goedjen <max.goedjen@gmail.com>
2020-07-29 23:17:36 -0700

    .

--- Source/GTMAppAuthFetcherAuthorization.m
+++ Source/GTMAppAuthFetcherAuthorization.m
@@ -19,7 +19,7 @@
 #import "GTMAppAuthFetcherAuthorization.h"
 
 #ifndef GTMAPPAUTH_USER_IMPORTS
-@import AppAuth;
+@import AppAuthCore;
 #else // GTMAPPAUTH_USER_IMPORTS
 #import "AppAuthCore.h"
 #endif // GTMAPPAUTH_USER_IMPORTS
